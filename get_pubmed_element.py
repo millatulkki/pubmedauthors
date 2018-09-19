c16850e5b93a9cd9b6a62299db8f05da0260d1a0
Milla <miltul@utu.fi>
2018-09-19 09:01:44 +0300

    finds the lastnames

--- get_pubmed_element.py
+++ get_pubmed_element.py
@@ -13,12 +13,32 @@ import xml.etree.ElementTree as ET
 def extract_sentences(in_xml):#, folder):
     tree = ET.parse(gzip.open(in_xml))
     docs = tree.getroot() # get root element
+    num = 0 # number of documents
     for doc in docs.findall('.//MedlineCitation'):
-        text = ''
-        doc_id = [item.text for item in doc.findall('PMID')][0]
-    #     print(doc_id)
-        year = [item.text for item in doc.findall('./DateCompleted/Year')]
-        print(doc_id,year)
+        num += 1
+
+        # for list in doc.findall('.//AuthorList'):
+        #     name = list.get('LastName')
+        #     print(name)
+
+    # tags = [elem.tag for elem in docs.iter()]
+
+        # text = ''
+        # authors = [a.text for a in doc.findall('Article/AuthorList/Author/LastName')]
+        # print(authors)
+
+        # for article in doc.iter('Author'): # the attributes of 'Author'
+        #     print(article.attrib)
+
+    print('Number of documents: ',str(num))
+
+### THESE WORK ###
+        # for item in doc.findall('PMID'):
+        #     print(item.text)
+
+        # text = ''
+        # doc_id = [item.text for item in doc.findall('PMID')][0]
+        # print(doc_id)
 
 def argument_parser():
     parser = argparse.ArgumentParser(description="extract sentence and title from pubmed elements")
